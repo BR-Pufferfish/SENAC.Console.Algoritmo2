766d3d6e192722e99069b76c211b07de8b6ea90f
Marcelo Ballin <puffersenac@gmail.com>
2025-03-25 20:42:09 -0300

    Inseridos exemplos de VAR

--- Algoritmo2/Algoritmo2/Program.cs
+++ Algoritmo2/Algoritmo2/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿    //Variáveis no C#
+
+int idade = 10;
+string nomeCliente = "Puffer";
+decimal preco = 10M;
+//o "M" após o 10 é pra indicar que ele é decimal, mesmo não tendo pontuação (ex: 10.0)
+char letra = 'p';
+var nomeCompleto = "BR Puffer Fish";
+var altura = 180;
